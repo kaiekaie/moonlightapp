b65958d855d238bec4b7f2c7bdbc03bd224aa313
Magnus <agamag15@student.westerdals.no>
2017-12-29 18:06:51 +0100

    sec

---
+++ test.js
@@ -0,0 +1,14 @@
+let largestring = 
+`Searching for server...
+Connect to 192.168.1.212...
+1. Steam
+2. JustCause3
+3. Arma 3
+4. Just Cause 3
+5. Hitman™
+6. DOTA 2
+7. Mad Max`
+
+let test = largestring.split("\n")
+console.log(test)
+
