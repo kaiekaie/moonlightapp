c2f21a1053eb57231dab7dcfcaa1125706c14c80
magnus <magnusaga@gmail.com>
2017-12-30 11:51:34 +0100

    removed sass

--- public/js.js
+++ public/js.js
@@ -1,103 +1,148 @@
+
+
 console.log("log")
 
 var socket = io();
 
-socket.on("list",function(data){
-  console.log(data)  
-})
+
 
 var hasGP = false;
 var repGP;
 
 function canGame() {
-    return "getGamepads" in navigator;
-}
-function checkkey(e){
-  console.log(e);
-
-
+  return "getGamepads" in navigator;
 }
 
 
 
-function controllLeftRight(num){
-  let coords = {};
-  if(num.length > 2){
-    coords = {"x1" : num[0], "y1" : num[1], "x2" :  num[2] , "y2" : num[3] }
+
+
+
+let lnf = 0;
+
+$(document).ready(function () {
+
+  let realFunction = function(e){
+   let lengt =  $("#gamelist li").length;
+  let arr = $("#gamelist li");
+ 
+        if(e === "left"){
+          lnf--;
+          if(lnf <= 1){
+            lnf = lengt - 1;
+          }
+
+          }else{
+            lnf++;
+            if(lnf >= lengt -1){
+              
+            inf = 0;
+            }
+
+          }
+
+          
+       
+
+
+
   }
 
-  if(coords.x1 === -1 || coords.x2 === -1){
-    console.log("left",num);
+  function controllLeftRight(num) {
+    let coords = {};
+    if (num.length > 2) {
+      coords = { "x1": num[0], "y1": num[1], "x2": num[2], "y2": num[3] }
+    }
+  
+    if (coords.x1 === -1 || coords.x2 === -1) {
+      realFunction("left")
+    }
+    if (coords.x1 === 1 || coords.x2 === 1) {
+  
+      realFunction("right")
+    }
+  
   }
-  if(coords.x1 === 1 || coords.x2 === 1){
 
-    console.log("right",num);
-  }
-
-}
-
-function reportOnGamepad() {
-  let coordinates = {};
+  function reportOnGamepad() {
+    let coordinates = {};
     var gp = navigator.getGamepads()[0];
     var html = "";
-        html += "id: "+gp.id+"<br/>";
-
-    for(var i=0;i<gp.buttons.length;i++) {
-        html+= "Button "+(i+1)+": ";
-        if(gp.buttons[i].pressed) html+= " pressed";
-        html+= "<br/>";
+    html += "id: " + gp.id + "<br/>";
+  
+    for (var i = 0; i < gp.buttons.length; i++) {
+      html += "Button " + (i + 1) + ": ";
+      if (gp.buttons[i].pressed) html += " pressed";
+      html += "<br/>";
     }
-
-    for(var i=0;i < gp.axes.length; i +=2) {
+  
+    for (var i = 0; i < gp.axes.length; i += 2) {
       controllLeftRight(gp.axes);
- 
-        html+= "Stick "+(Math.ceil(i/2)+1)+": "+gp.axes[i]+","+gp.axes[i+1]+"<br/>";
+  
+      html += "Stick " + (Math.ceil(i / 2) + 1) + ": " + gp.axes[i] + "," + gp.axes[i + 1] + "<br/>";
     }
-
+  
     $("#gamepadDisplay").html(html);
+  }
+function checkkey(e) {
+
+  if (e === "ArrowLeft") {
+    realFunction("left")
+
+          }
+          if (e === "ArrowRight") {
+    
+            realFunction("right")
+          }
+
+
+
 }
 
-$(document).ready(function() {
 
-    if(canGame()) {
-
-        var prompt = "To begin using your gamepad, connect it and press any button!";
-        $("#gamepadPrompt").text(prompt);
-
-        $(window).on("gamepadconnected", function() {
-            hasGP = true;
-            $("#gamepadPrompt").html("Gamepad connected!");
-            console.log("connection event");
-            repGP = window.setInterval(reportOnGamepad,100);
-        });
+  socket.on("list", function (data) {
+    console.log(data)
+    data.forEach((element,i) => {
   
-        $(window).on("keydown", function(e) {
-         checkkey(e.originalEvent.key);
-         if(e.originalEvent.key === "ArrowLeft"){
+    
+      $("#gamelist").append(`<li>${element}</li>`);
+    });
 
 
-         }
-         if(e.originalEvent.key === "ArrowRight"){
-          
-          
-         }
+  })
 
+  if (canGame()) {
 
-        })
-        $(window).on("gamepaddisconnected", function() {
-            console.log("disconnection event");
-            $("#gamepadPrompt").text(prompt);
-            window.clearInterval(repGP);
-        });
+    var prompt = "To begin using your gamepad, connect it and press any button!";
+    $("#gamepadPrompt").text(prompt);
 
-        //setup an interval for Chrome
-        var checkGP = window.setInterval(function() {
-            console.log('checkGP');
-            if(navigator.getGamepads()[0]) {
-                if(!hasGP) $(window).trigger("gamepadconnected");
-                window.clearInterval(checkGP);
-            }
-        }, 500);
-    }
+    $(window).on("gamepadconnected", function () {
+      hasGP = true;
+      $("#gamepadPrompt").html("Gamepad connected!");
+      console.log("connection event");
+      repGP = window.setInterval(reportOnGamepad, 100);
+    });
+
+    $(window).on("keydown", function (e) {
+      checkkey(e.originalEvent.key);
+  
+
+    })
+
+    $(window).on("gamepaddisconnected", function () {
+      console.log("disconnection event");
+      $("#gamepadPrompt").text(prompt);
+      window.clearInterval(repGP);
+    });
+
+    //setup an interval for Chrome
+    var checkGP = window.setInterval(function () {
+      console.log('checkGP');
+      if (navigator.getGamepads()[0]) {
+        if (!hasGP) $(window).trigger("gamepadconnected");
+        window.clearInterval(checkGP);
+      }
+    }, 500);
+  }
 
 });
--- test.bat
+++
@@ -1,9 +0,0 @@
-echo Searching for server.../n
-"Connect to 192.168.1.212...""
-"1. Steam"
-"2. JustCause3"
-"3. Arma 3"
-"4. Just Cause 3"
-"5. Hitman™"
-"6. DOTA 2"
-"7. Mad Max"
